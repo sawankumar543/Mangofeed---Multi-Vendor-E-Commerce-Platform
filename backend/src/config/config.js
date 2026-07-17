f114fea2ae0144b61b9de35aecc684b1bb7f0226
sawankumar543 <devnagricode@gmail.com>
2026-07-16 21:32:08 -0700

    feat(auth) created generateAccessToken method

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -10,6 +10,10 @@ const config = {
     DB_URL: process.env.MONGODB_URI,
     CLIENT_URL: process.env.CLIENT_URL,
     BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+    ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+    REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN,
 }
 
 export default config
